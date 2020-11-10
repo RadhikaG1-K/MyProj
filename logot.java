f33be02b74ad9701540dd4ba434eeb0a71cd1fa5
root <root@node2.europe-west2-c.c.semiotic-pager-294514.internal>
2020-11-10 13:07:17 +0000

    Exit menu changes added by Dev2@Node 2

---
+++ logot.java
@@ -0,0 +1,2 @@
+This is Logout Screen When user wants to exit the screen , dont click on 'X' mark on the top of the window. Click on Logout button displayed user under Exit Menu that way USer will bot loose any unsaved data.
+
